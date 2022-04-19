4f268060c08815059f491506e60df56dd5890a24
Lily <helloimlily@gmail.com>
2022-04-19 14:18:10 +0900

    #3.1 Conneting the Store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
