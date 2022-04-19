87cd4e1a881f23336231d242f7ca72743919a219
Lily <helloimlily@gmail.com>
2022-04-19 14:34:12 +0900

    #3.2 mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -22,4 +23,8 @@ function Home() {
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
